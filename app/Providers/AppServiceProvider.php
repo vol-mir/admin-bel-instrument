7541f07f9d2ee79599af753c966e31618fbd9fea
Vladzimir Bykovski <v.bykovski@e-vision.by>
2023-05-07 13:52:11 +0300

    Add setting name to title

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -4,6 +4,7 @@ declare(strict_types=1);
 
 namespace App\Providers;
 
+use App\Models\Setting;
 use Illuminate\Support\ServiceProvider;
 
 class AppServiceProvider extends ServiceProvider
@@ -21,6 +22,6 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
-        //
+        view()->share('settings', Setting::where('slug', 'base')->first());
     }
 }
